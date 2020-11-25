b8bd28ba2c9a22731f2cab146fe31430b6a91fca
spoonLead <xuxlaevoleg@yandex.ru>
2020-11-25 10:55:28 +0300

    Fix: using global vars in scanPorts

--- portScanner.py
+++ portScanner.py
@@ -36,11 +36,11 @@ def printHostIpAndPortRange():
 
 
 def scanPorts():
-    for port in range(1,1024):
+    for port in range(1,MAX_PORT):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.settimeout(1000)
-            s.connect(('127.0.0.1', port))
+            s.connect((HOST_IP, port))
             print(port)
             s.close
         except: continue
